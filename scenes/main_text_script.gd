367e430bfb8350111b9fb8916f0241cc0627e7b2
Andrei Bondad <jabondad24@gmail.com>
2025-12-18 22:22:11 -0600

    Added text printing to screen

--- scenes/main_text_script.gd
+++ scenes/main_text_script.gd
@@ -1,1 +1,18 @@
 extends RichTextLabel
+
+var testText = "Test Text"
+var testTextLength = testText.length();
+var count = 0;
+
+func _ready():
+	add_text("Hello")
+
+func _printText():
+	pass
+
+
+func _on_timer_timeout() -> void:
+	if (count < testTextLength):
+		append_text(testText[count]);
+		count = count + 1
+	
