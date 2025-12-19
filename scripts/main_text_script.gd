f22565bb2fcea81f8cc1a5393a1a23c994025d34
Andrei Bondad <jabondad24@gmail.com>
2025-12-19 12:54:46 -0600

    Created a way for buttons to modify the displayed text

--- scripts/main_text_script.gd
+++ scripts/main_text_script.gd
@@ -22,3 +22,10 @@ func _on_timer_timeout() -> void:
 		append_text(testText[count]);
 		count = count + 1
 	
+
+
+func _on_choice_1_pressed() -> void:
+	testText = "Choice 1 was selected"
+	var testTextLength = testText.length();
+	self.text = ""
+	count = 0
