72131e3d94b2d2cf986674a30aff00f230931c7c
Andrei Bondad <jabondad24@gmail.com>
2025-12-19 13:11:27 -0600

    Changed variable names

--- scripts/main_text_script.gd
+++ scripts/main_text_script.gd
@@ -1,7 +1,7 @@
 extends RichTextLabel
 
-var testText = "Test Text Test Text Test Text Test Text Test Text"
-var testTextLength = testText.length();
+var displayText = "Test Text Test Text Test Text Test Text Test Text"
+var displayTextLength = displayText.length();
 var count = 0;
 
 func _ready():
@@ -13,19 +13,19 @@ func _printText():
 
 func _process(delta: float) -> void:
 	if Input.is_action_pressed("skip"):
-		count = testTextLength + 1
-		self.text = testText
+		count = displayTextLength + 1
+		self.text = displayText
 
 func _on_timer_timeout() -> void:
 	
-	if (count < testTextLength):
-		append_text(testText[count]);
+	if (count < displayTextLength):
+		append_text(displayText[count]);
 		count = count + 1
 	
 
 
 func _on_choice_1_pressed() -> void:
-	testText = "Choice 1 was selected"
-	var testTextLength = testText.length();
+	displayText = "Choice 1 was selected"
+	displayTextLength = displayText.length();
 	self.text = ""
 	count = 0
