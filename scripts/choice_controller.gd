9f282345a9f98a49b556e1d7a2877a4976ecd534
Andrei Bondad <jabondad24@gmail.com>
2025-12-24 17:30:33 -0600

    Made text boxes disappear if game over is achieved

--- scripts/choice_controller.gd
+++ scripts/choice_controller.gd
@@ -2,7 +2,9 @@ extends VBoxContainer
 
 
 func _on_main_text_game_over() -> void:
-	print("game over")
 	$Choice1.disabled = true
+	$Choice1.visible = false
 	$Choice2.disabled = true
+	$Choice2.visible = false
 	$Choice3.disabled = true
+	$Choice3.visible = false
