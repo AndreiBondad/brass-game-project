86d86fa5fb4ad97e0ad215e0b34db0abe48b242e
Andrei Bondad <jabondad24@gmail.com>
2025-12-24 15:12:11 -0600

    Implemented Text Controller Class and dialogue graph framework

--- scripts/main_text_script.gd
+++ scripts/main_text_script.gd
@@ -1,17 +1,27 @@
 extends RichTextLabel
 
-var startingNode = TextNode.new()
-
-var displayText = startingNode.currentText
-var choiceOneText = startingNode.optionA
-var choiceTwoText = startingNode.optionB
-var choiceThreeText = startingNode.optionC
-var displayTextLength = displayText.length();
+var currentNode
+var displayText
+var choiceOneNode
+var choiceTwoNode
+var choiceThreeNode
+var displayTextLength
 var count = 0;
 
 func _ready():
-	#add_text("Hello")
-	pass
+	var startingNode = TextNode.new("This is the opening text")
+	currentNode = startingNode
+	startingNode.set_options(TextNode.new("Option A was chosen"), TextNode.new("Option B was chosen"), TextNode.new("Option C was chosen"))
+	startingNode.optionList[0].set_options(TextNode.new("Option AA was chosen"), TextNode.new("Option AB was chosen"), TextNode.new("Option AC was chosen"))
+	startingNode.optionList[1].set_options(TextNode.new("Option BA was chosen"), TextNode.new("Option BB was chosen"), TextNode.new("Option BC was chosen"))
+	startingNode.optionList[2].set_options(TextNode.new("Option CA was chosen"), TextNode.new("Option CB was chosen"), TextNode.new("Option CC was chosen"))
+	
+	displayText = startingNode.currentText
+	choiceOneNode = startingNode.optionList[0]
+	choiceTwoNode = startingNode.optionList[1]
+	choiceThreeNode = startingNode.optionList[2]
+	
+	displayTextLength = displayText.length();
 
 func _printText():
 	pass
@@ -22,7 +32,6 @@ func _process(delta: float) -> void:
 		self.text = displayText
 
 func _on_timer_timeout() -> void:
-	
 	if (count < displayTextLength):
 		append_text(displayText[count]);
 		count = count + 1
@@ -30,21 +39,24 @@ func _on_timer_timeout() -> void:
 
 
 func _on_choice_1_pressed() -> void:
-	displayText = choiceOneText
+	currentNode = currentNode.optionList[0]
+	displayText = currentNode.currentText
 	displayTextLength = displayText.length();
 	self.text = ""
 	count = 0
 
 
 func _on_choice_2_pressed() -> void:
-	displayText = choiceTwoText
+	currentNode = currentNode.optionList[1]
+	displayText = currentNode.currentText
 	displayTextLength = displayText.length();
 	self.text = ""
 	count = 0
 
 
 func _on_choice_3_pressed() -> void:
-	displayText = choiceThreeText
+	currentNode = currentNode.optionList[2]
+	displayText = currentNode.currentText
 	displayTextLength = displayText.length();
 	self.text = ""
 	count = 0
